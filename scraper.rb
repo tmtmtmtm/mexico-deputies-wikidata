693403ce1b99cf6d5bc37123f5435804aff21d58
Tony Bowden <tony@tmtm.com>
2015-08-18 23:07:15 +0300

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,30 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'json'
+require 'pry'
+require 'rest-client'
+require 'scraperwiki'
+require 'wikidata/fetcher'
+require 'mediawiki_api'
+
+def candidates
+  morph_api_url = 'https://api.morph.io/tmtmtmtm/mexico-deputies-wikipedia/data.json'
+  morph_api_key = ENV["MORPH_API_KEY"]
+  result = RestClient.get morph_api_url, params: {
+    key: morph_api_key,
+    query: "select DISTINCT(wikipedia__en) as wikiname from data"
+  }
+  JSON.parse(result, symbolize_names: true)
+end
+
+WikiData.ids_from_pages('lb', candidates.map { |c| c[:wikiname] }).each_with_index do |p, i|
+  puts i if (i % 50).zero?
+  data = WikiData::Fetcher.new(id: p.last).data('es') rescue nil
+  unless data
+    warn "No data for #{p}"
+    next
+  end
+  ScraperWiki.save_sqlite([:id], data)
+end
+
