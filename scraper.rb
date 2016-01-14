cec8d545b006dda9e81a515ee8123f03a2ed1387
Tony Bowden <tony@tmtm.com>
2016-01-14 20:38:02 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,32 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'json'
 require 'pry'
-require 'rest-client'
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'mediawiki_api'
 
-def members
-  morph_api_url = 'https://api.morph.io/tmtmtmtm/mexico-deputies-wikipedia/data.json'
-  morph_api_key = ENV["MORPH_API_KEY"]
-  result = RestClient.get morph_api_url, params: {
-    key: morph_api_key,
-    query: "select DISTINCT(wikipedia__en) as wikiname from data"
-  }
-  JSON.parse(result, symbolize_names: true)
-end
-
-WikiData.ids_from_pages('es', members.map { |c| c[:wikiname] }).each_with_index do |p, i|
-  data = WikiData::Fetcher.new(id: p.last).data('es') rescue nil
-  unless data
-    warn "No data for #{p}"
-    next
-  end
-  data[:original_wikiname] = p.first
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/mexico-deputies-wikipedia', column: 'wikipedia__en')
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+EveryPolitician::Wikidata.notify_rebuilder
 
