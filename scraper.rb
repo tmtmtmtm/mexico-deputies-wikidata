e5dd5b35a37239b274c101a4419504f5918e6066
Tony Bowden <tony@tmtm.com>
2017-01-07 11:43:20 +0000

    Bypass Wikipedia scraper, and get names directly
    Just load the data directly from the Wikipedia page

--- scraper.rb
+++ scraper.rb
@@ -4,5 +4,11 @@
 require 'pry'
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/mexico-deputies-wikipedia', column: 'wikipedia__en')
+names = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/LXII_Legislature_of_the_Mexican_Congress',
+  after: '//span[@id="Deputies_of_the_LXII_Legislature"]',
+  before: '//span[@id="References"]',
+  xpath: '//table//tr[td]//td[position()=2 or position()=5]//a[not(@class="new")]/@title',
+)
+
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
