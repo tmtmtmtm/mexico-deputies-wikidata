a77c374249ab2c784cdb427275ca8f82a94a8e11
Tony Bowden <tony@tmtm.com>
2018-08-17 21:02:50 +0100

    Also get via eswiki Categories

--- scraper.rb
+++ scraper.rb
@@ -4,11 +4,14 @@
 require 'pry'
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath(
+es_cat = WikiData::Category.new('Categoría:Diputados_de_la_LXII_Legislatura_de_México', 'es').member_titles |
+         WikiData::Category.new('Categoría:Diputados_de_la_LXIII_Legislatura_de_México', 'es').member_titles
+
+en_names = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/LXII_Legislature_of_the_Mexican_Congress',
   after: '//span[@id="Deputies"]',
   before: '//span[@id="References"]',
   xpath: '//table//tr[td]//td[position()=2 or position()=5]//a[not(@class="new")]/@title',
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+EveryPolitician::Wikidata.scrape_wikidata(names: { es: es_cat, en: en_names })
