516b31957a192d700ac75935b7ee322cbeda018f
Tony Bowden <tony@tmtm.com>
2018-11-21 10:03:07 +0000

    Also get via enwiki 63

--- scraper.rb
+++ scraper.rb
@@ -7,10 +7,24 @@ require 'wikidata/fetcher'
 es_cat = WikiData::Category.new('Categoría:Diputados_de_la_LXII_Legislatura_de_México', 'es').member_titles |
          WikiData::Category.new('Categoría:Diputados_de_la_LXIII_Legislatura_de_México', 'es').member_titles
 
-en_names = EveryPolitician::Wikidata.wikipedia_xpath(
+en_62 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/LXII_Legislature_of_the_Mexican_Congress',
   after: '//span[@id="Deputies"]',
   before: '//span[@id="References"]',
+  xpath: '//table//tr[td]//td[position()=2 or position()=5]//a[not(@class="new")]/@title',
+)
+
+en_63_area = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/LXIII_Legislature_of_the_Mexican_Congress',
+  after: '//span[@id="Deputies_by_single-member_district_(relative_majority)"]',
+  before: '//span[@id="Deputies_by_proportional_representation"]',
+  xpath: '//table//tr[td]//td[position()=3 or position()=7]//a[not(@class="new")]/@title',
+)
+
+en_63_pr = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/LXIII_Legislature_of_the_Mexican_Congress',
+  after: '//span[@id="Deputies_by_proportional_representation"]',
+  before: '//span[@id="See_also"]',
   xpath: '//table//tr[td]//td[position()=2 or position()=5]//a[not(@class="new")]/@title',
 )
 
@@ -23,4 +37,4 @@ query = <<SPARQL
 SPARQL
 p39s = EveryPolitician::Wikidata.sparql(query)
 
-EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { es: es_cat, en: en_names })
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { es: es_cat, en: en_62 | en_63_area | en_63_pr })
